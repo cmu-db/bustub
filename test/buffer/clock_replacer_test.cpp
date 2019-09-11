89dcdde1084aaf2a73225793fc61cfbef77486f4
Wan Shen Lim <wanshen.lim@gmail.com>
2019-09-11 12:38:02 -0400

    Format...

--- test/buffer/clock_replacer_test.cpp
+++ test/buffer/clock_replacer_test.cpp
@@ -11,8 +11,8 @@
 //===----------------------------------------------------------------------===//
 
 #include <cstdio>
-#include <vector>
 #include <thread>
+#include <vector>
 
 #include "buffer/clock_replacer.h"
 #include "gtest/gtest.h"
@@ -21,7 +21,7 @@ namespace bustub {
 
 TEST(ClockReplacerTest, DISABLED_SampleTest) {
   ClockReplacer clock_replacer(7);
-  
+
   // Scenario: unpin six elements, i.e. add them to the replacer.
   clock_replacer.Unpin(1);
   clock_replacer.Unpin(2);
@@ -31,7 +31,7 @@ TEST(ClockReplacerTest, DISABLED_SampleTest) {
   clock_replacer.Unpin(6);
   clock_replacer.Unpin(1);
   EXPECT_EQ(6, clock_replacer.Size());
-  
+
   // Scenario: get three victims from the clock.
   int value;
   clock_replacer.Victim(&value);
@@ -40,13 +40,13 @@ TEST(ClockReplacerTest, DISABLED_SampleTest) {
   EXPECT_EQ(2, value);
   clock_replacer.Victim(&value);
   EXPECT_EQ(3, value);
-  
+
   // Scenario: pin elements in the replacer.
   // Note that 3 has already been victimized, so pinning 3 should have no effect.
   clock_replacer.Pin(3);
   clock_replacer.Pin(4);
   EXPECT_EQ(2, clock_replacer.Size());
-  
+
   // Scenario: unpin 4. We expect that the reference bit of 4 will be set to 1.
   clock_replacer.Unpin(4);
 
@@ -59,4 +59,4 @@ TEST(ClockReplacerTest, DISABLED_SampleTest) {
   EXPECT_EQ(4, value);
 }
 
-} // namespace bustub
+}  // namespace bustub
