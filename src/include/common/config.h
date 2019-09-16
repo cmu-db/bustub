6260be846c2943a022c22233286001067847c556
Tulio Leao <tupaschoal@gmail.com>
2019-09-16 00:08:08 -0300

    Fix clang-format lint issues

--- src/include/common/config.h
+++ src/include/common/config.h
@@ -27,14 +27,14 @@ extern std::atomic<bool> ENABLE_LOGGING;
 /** If ENABLE_LOGGING is true, the log should be flushed to disk every LOG_TIMEOUT. */
 extern std::chrono::duration<int64_t> LOG_TIMEOUT;
 
-static constexpr int INVALID_PAGE_ID = 1;                                    // representing an invalid page id
-static constexpr int INVALID_TXN_ID = -1;                                    // representing an invalid transaction id
-static constexpr int INVALID_LSN = -1;                                       // representing an invalid log sequence number
-static constexpr int HEADER_PAGE_ID = 0;                                     // the header page id
-static constexpr int PAGE_SIZE = 4096;                                       // size of a data page in byte
-static constexpr int BUFFER_POOL_SIZE = 10;                                  // size of buffer pool
-static constexpr int LOG_BUFFER_SIZE = ((BUFFER_POOL_SIZE + 1) * PAGE_SIZE); // size of a log buffer in byte
-static constexpr int BUCKET_SIZE = 50;                                       // size of extendible hash bucket
+static constexpr int INVALID_PAGE_ID = 1;                                     // invalid page id
+static constexpr int INVALID_TXN_ID = -1;                                     // invalid transaction id
+static constexpr int INVALID_LSN = -1;                                        // invalid log sequence number
+static constexpr int HEADER_PAGE_ID = 0;                                      // the header page id
+static constexpr int PAGE_SIZE = 4096;                                        // size of a data page in byte
+static constexpr int BUFFER_POOL_SIZE = 10;                                   // size of buffer pool
+static constexpr int LOG_BUFFER_SIZE = ((BUFFER_POOL_SIZE + 1) * PAGE_SIZE);  // size of a log buffer in byte
+static constexpr int BUCKET_SIZE = 50;                                        // size of extendible hash bucket
 
 
 using frame_id_t = int32_t;  // frame id type
