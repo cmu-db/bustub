abcb772a012077c7d1f7a0ed59ded2100dafe0b3
wenxuanqiu <bobbyqiub@gmail.com>
2019-09-28 18:50:10 -0400

    Update hash_table_header_page.cpp

--- src/storage/page/hash_table_header_page.cpp
+++ src/storage/page/hash_table_header_page.cpp
@@ -13,36 +13,18 @@
 #include "storage/page/hash_table_header_page.h"
 
 namespace bustub {
-page_id_t HashTableHeaderPage::GetBlockPageId(size_t index) {
-  // TODO
-  return 0;
-}
+page_id_t HashTableHeaderPage::GetBlockPageId(size_t index) { return 0; }
 
-page_id_t HashTableHeaderPage::GetPageId() const {
-  // TODO
-  return 0;
-}
+page_id_t HashTableHeaderPage::GetPageId() const { return 0; }
 
-void HashTableHeaderPage::SetPageId(bustub::page_id_t page_id) {
-  // TODO
-}
+void HashTableHeaderPage::SetPageId(bustub::page_id_t page_id) {}
 
-void HashTableHeaderPage::AddBlockPageId(page_id_t page_id) {
-  // TODO
-}
+void HashTableHeaderPage::AddBlockPageId(page_id_t page_id) {}
 
-size_t HashTableHeaderPage::NumBlocks() {
-  // TODO
-  return 0;
-}
+size_t HashTableHeaderPage::NumBlocks() { return 0; }
 
-void HashTableHeaderPage::SetSize(size_t size) {
-  // TODO
-}
+void HashTableHeaderPage::SetSize(size_t size) {}
 
-size_t HashTableHeaderPage::GetSize() const {
-  // TODO
-  return 0;
-}
+size_t HashTableHeaderPage::GetSize() const { return 0; }
 
 }  // namespace bustub
