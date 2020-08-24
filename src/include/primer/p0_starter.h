ac5ad08ff0617db0def12508c1069b33d0168cb8
gautam20197 <gautam20197@gmail.com>
2020-08-24 01:50:15 -0400

    Fixes based on the review, function names, TODO and terrier clang

--- src/include/primer/p0_starter.h
+++ src/include/primer/p0_starter.h
@@ -20,7 +20,7 @@
 template <typename T>
 class Matrix {
  protected:
-  // P0: Add implementation
+  // TODO(P0): Add implementation
   Matrix(int r, int c) {}
 
   // # of rows in the matrix
@@ -28,60 +28,60 @@ class Matrix {
   // # of Columns in the matrix
   int cols;
   // Flattened array containing the elements of the matrix
-  // P0: Allocate the array in the constructor. Don't forget to free up
+  // TODO(P0) : Allocate the array in the constructor. Don't forget to free up
   // the array in the destructor.
   T *linear;
 
  public:
   // Return the # of rows in the matrix
-  virtual int getRows() = 0;
+  virtual int GetRows() = 0;
 
   // Return the # of columns in the matrix
-  virtual int getColumns() = 0;
+  virtual int GetColumns() = 0;
 
   // Return the (i,j)th  matrix element
-  virtual T getElem(int i, int j) = 0;
+  virtual T GetElem(int i, int j) = 0;
 
   // Sets the (i,j)th  matrix element to val
-  virtual void setElem(int i, int j, T val) = 0;
+  virtual void SetElem(int i, int j, T val) = 0;
 
   // Sets the matrix elements based on the array arr
-  virtual void matImport(T *arr) = 0;
+  virtual void MatImport(T *arr) = 0;
 
-  // P0: Add implementation
+  // TODO(P0): Add implementation
   virtual ~Matrix() = default;
 };
 
 template <typename T>
 class RowMatrix : public Matrix<T> {
  public:
-  // P0: Add implementation
+  // TODO(P0): Add implementation
   RowMatrix(int r, int c) : Matrix<T>(r, c) {}
 
-  // P0: Add implementation
-  int getRows() override { return 0; }
+  // TODO(P0): Add implementation
+  int GetRows() override { return 0; }
 
-  // P0: Add implementation
-  int getColumns() override { return 0; }
+  // TODO(P0): Add implementation
+  int GetColumns() override { return 0; }
 
-  // P0: Add implementation
-  T getElem(int i, int j) override { return data[i][j]; }
+  // TODO(P0): Add implementation
+  T GetElem(int i, int j) override { return data_[i][j]; }
 
-  // P0: Add implementation
-  void setElem(int i, int j, T val) override {}
+  // TODO(P0): Add implementation
+  void SetElem(int i, int j, T val) override {}
 
-  // P0: Add implementation
-  void matImport(T *arr) override {}
+  // TODO(P0): Add implementation
+  void MatImport(T *arr) override {}
 
-  // P0: Add implementation
+  // TODO(P0): Add implementation
   ~RowMatrix() override = default;
 
  private:
   // 2D array containing the elements of the matrix in row-major format
-  // P0: Allocate the array of row pointers in the constructor. Use these pointers
+  // TODO(P0): Allocate the array of row pointers in the constructor. Use these pointers
   // to point to corresponding elements of the 'linear' array.
   // Don't forget to free up the array in the destructor.
-  T **data;
+  T **data_;
 };
 
 template <typename T>
@@ -89,28 +89,28 @@ class RowMatrixOperations {
  public:
   // Compute (mat1 + mat2) and return the result.
   // Return nullptr if dimensions mismatch for input matrices.
-  static std::unique_ptr<RowMatrix<T>> addMatrices(std::unique_ptr<RowMatrix<T>> mat1,
+  static std::unique_ptr<RowMatrix<T>> AddMatrices(std::unique_ptr<RowMatrix<T>> mat1,
                                                    std::unique_ptr<RowMatrix<T>> mat2) {
-    // P0: Add code
+    // TODO(P0): Add code
 
     return std::unique_ptr<RowMatrix<T>>(nullptr);
   }
 
   // Compute matrix multiplication (mat1 * mat2) and return the result.
   // Return nullptr if dimensions mismatch for input matrices.
-  static std::unique_ptr<RowMatrix<T>> multiplyMatrices(std::unique_ptr<RowMatrix<T>> mat1,
+  static std::unique_ptr<RowMatrix<T>> MultiplyMatrices(std::unique_ptr<RowMatrix<T>> mat1,
                                                         std::unique_ptr<RowMatrix<T>> mat2) {
-    // P0: Add code
+    // TODO(P0): Add code
 
     return std::unique_ptr<RowMatrix<T>>(nullptr);
   }
 
   // Simplified GEMM (general matrix multiply) operation
   // Compute (matA * matB + matC). Return nullptr if dimensions mismatch for input matrices
-  static std::unique_ptr<RowMatrix<T>> gemmMatrices(std::unique_ptr<RowMatrix<T>> matA,
+  static std::unique_ptr<RowMatrix<T>> GemmMatrices(std::unique_ptr<RowMatrix<T>> matA,
                                                     std::unique_ptr<RowMatrix<T>> matB,
                                                     std::unique_ptr<RowMatrix<T>> matC) {
-    // P0: Add code
+    // TODO(P0): Add code
 
     return std::unique_ptr<RowMatrix<T>>(nullptr);
   }
