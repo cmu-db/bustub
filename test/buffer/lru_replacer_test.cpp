8969bd32b10195ddb1af78fffd528044601cec4e
Rohan Aggarwal <rohanagg@andrew.cmu.edu>
2020-08-30 22:44:43 -0400

    Retain clock

--- test/buffer/lru_replacer_test.cpp
+++ test/buffer/lru_replacer_test.cpp
@@ -1,11 +1,10 @@
-
 //===----------------------------------------------------------------------===//
 //
 //                         BusTub
 //
-// clock_replacer_test.cpp
+// lru_replacer_test.cpp
 //
-// Identification: test/buffer/clock_replacer_test.cpp
+// Identification: test/buffer/lru_replacer_test.cpp
 //
 // Copyright (c) 2015-2019, Carnegie Mellon University Database Group
 //
@@ -15,48 +14,48 @@
 #include <thread>  // NOLINT
 #include <vector>
 
-#include "buffer/clock_replacer.h"
+#include "buffer/lru_replacer.h"
 #include "gtest/gtest.h"
 
 namespace bustub {
 
-TEST(ClockReplacerTest, DISABLED_SampleTest) {
-  ClockReplacer clock_replacer(7);
+TEST(LRUReplacerTest, DISABLED_SampleTest) {
+  LRUReplacer lru_replacer(7);
 
   // Scenario: unpin six elements, i.e. add them to the replacer.
-  clock_replacer.Unpin(1);
-  clock_replacer.Unpin(2);
-  clock_replacer.Unpin(3);
-  clock_replacer.Unpin(4);
-  clock_replacer.Unpin(5);
-  clock_replacer.Unpin(6);
-  clock_replacer.Unpin(1);
-  EXPECT_EQ(6, clock_replacer.Size());
+  lru_replacer.Unpin(1);
+  lru_replacer.Unpin(2);
+  lru_replacer.Unpin(3);
+  lru_replacer.Unpin(4);
+  lru_replacer.Unpin(5);
+  lru_replacer.Unpin(6);
+  lru_replacer.Unpin(1);
+  EXPECT_EQ(6, lru_replacer.Size());
 
-  // Scenario: get three victims from the clock.
+  // Scenario: get three victims from the lru.
   int value;
-  clock_replacer.Victim(&value);
+  lru_replacer.Victim(&value);
   EXPECT_EQ(1, value);
-  clock_replacer.Victim(&value);
+  lru_replacer.Victim(&value);
   EXPECT_EQ(2, value);
-  clock_replacer.Victim(&value);
+  lru_replacer.Victim(&value);
   EXPECT_EQ(3, value);
 
   // Scenario: pin elements in the replacer.
   // Note that 3 has already been victimized, so pinning 3 should have no effect.
-  clock_replacer.Pin(3);
-  clock_replacer.Pin(4);
-  EXPECT_EQ(2, clock_replacer.Size());
+  lru_replacer.Pin(3);
+  lru_replacer.Pin(4);
+  EXPECT_EQ(2, lru_replacer.Size());
 
   // Scenario: unpin 4. We expect that the reference bit of 4 will be set to 1.
-  clock_replacer.Unpin(4);
+  lru_replacer.Unpin(4);
 
   // Scenario: continue looking for victims. We expect these victims.
-  clock_replacer.Victim(&value);
+  lru_replacer.Victim(&value);
   EXPECT_EQ(5, value);
-  clock_replacer.Victim(&value);
+  lru_replacer.Victim(&value);
   EXPECT_EQ(6, value);
-  clock_replacer.Victim(&value);
+  lru_replacer.Victim(&value);
   EXPECT_EQ(4, value);
 }
 
