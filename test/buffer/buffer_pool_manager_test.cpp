6f66e3eb48fb153d03d964b100a71f3ecc8210fd
rammiah <rammiahcn@gmail.com>
2020-09-04 09:59:29 +0800

    enable test

--- test/buffer/buffer_pool_manager_test.cpp
+++ test/buffer/buffer_pool_manager_test.cpp
@@ -19,7 +19,7 @@ namespace bustub {
 
 // NOLINTNEXTLINE
 // Check whether pages containing terminal characters can be recovered
-TEST(BufferPoolManagerTest, DISABLED_BinaryDataTest) {
+TEST(BufferPoolManagerTest, BinaryDataTest) {
   const std::string db_name = "test.db";
   const size_t buffer_pool_size = 10;
 
