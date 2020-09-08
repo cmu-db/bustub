88094726d5f34d5baff7e4a777ac135c6bc8fe61
Rohan Aggarwal <rohanagg@andrew.cmu.edu>
2020-09-07 21:52:38 -0400

    Add LRUReplacer Infrastructure (#87)

---
+++ src/include/buffer/lru_replacer.h
@@ -0,0 +1,52 @@
+//===----------------------------------------------------------------------===//
+//
+//                         BusTub
+//
+// lru_replacer.h
+//
+// Identification: src/include/buffer/lru_replacer.h
+//
+// Copyright (c) 2015-2019, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <list>
+#include <mutex>  // NOLINT
+#include <vector>
+
+#include "buffer/replacer.h"
+#include "common/config.h"
+
+namespace bustub {
+
+/**
+ * LRUReplacer implements the lru replacement policy, which approximates the Least Recently Used policy.
+ */
+class LRUReplacer : public Replacer {
+ public:
+  /**
+   * Create a new LRUReplacer.
+   * @param num_pages the maximum number of pages the LRUReplacer will be required to store
+   */
+  explicit LRUReplacer(size_t num_pages);
+
+  /**
+   * Destroys the LRUReplacer.
+   */
+  ~LRUReplacer() override;
+
+  bool Victim(frame_id_t *frame_id) override;
+
+  void Pin(frame_id_t frame_id) override;
+
+  void Unpin(frame_id_t frame_id) override;
+
+  size_t Size() override;
+
+ private:
+  // TODO(student): implement me!
+};
+
+}  // namespace bustub
---
+++ test/buffer/lru_replacer_test.cpp
@@ -0,0 +1,62 @@
+//===----------------------------------------------------------------------===//
+//
+//                         BusTub
+//
+// lru_replacer_test.cpp
+//
+// Identification: test/buffer/lru_replacer_test.cpp
+//
+// Copyright (c) 2015-2019, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#include <cstdio>
+#include <thread>  // NOLINT
+#include <vector>
+
+#include "buffer/lru_replacer.h"
+#include "gtest/gtest.h"
+
+namespace bustub {
+
+TEST(LRUReplacerTest, DISABLED_SampleTest) {
+  LRUReplacer lru_replacer(7);
+
+  // Scenario: unpin six elements, i.e. add them to the replacer.
+  lru_replacer.Unpin(1);
+  lru_replacer.Unpin(2);
+  lru_replacer.Unpin(3);
+  lru_replacer.Unpin(4);
+  lru_replacer.Unpin(5);
+  lru_replacer.Unpin(6);
+  lru_replacer.Unpin(1);
+  EXPECT_EQ(6, lru_replacer.Size());
+
+  // Scenario: get three victims from the lru.
+  int value;
+  lru_replacer.Victim(&value);
+  EXPECT_EQ(1, value);
+  lru_replacer.Victim(&value);
+  EXPECT_EQ(2, value);
+  lru_replacer.Victim(&value);
+  EXPECT_EQ(3, value);
+
+  // Scenario: pin elements in the replacer.
+  // Note that 3 has already been victimized, so pinning 3 should have no effect.
+  lru_replacer.Pin(3);
+  lru_replacer.Pin(4);
+  EXPECT_EQ(2, lru_replacer.Size());
+
+  // Scenario: unpin 4. We expect that the reference bit of 4 will be set to 1.
+  lru_replacer.Unpin(4);
+
+  // Scenario: continue looking for victims. We expect these victims.
+  lru_replacer.Victim(&value);
+  EXPECT_EQ(5, value);
+  lru_replacer.Victim(&value);
+  EXPECT_EQ(6, value);
+  lru_replacer.Victim(&value);
+  EXPECT_EQ(4, value);
+}
+
+}  // namespace bustub
