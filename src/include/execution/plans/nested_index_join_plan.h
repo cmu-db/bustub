e8251c2df03e60f5a8e679d2db49ff7658865820
Ricky Zhou <zhou0ricky@gmail.com>
2020-11-07 17:34:06 -0500

    updated nested index join plan

--- src/include/execution/plans/nested_index_join_plan.h
+++ src/include/execution/plans/nested_index_join_plan.h
@@ -30,34 +30,41 @@ namespace bustub {
 class NestedIndexJoinPlanNode : public AbstractPlanNode {
  public:
   NestedIndexJoinPlanNode(const Schema *output_schema, std::vector<const AbstractPlanNode *> &&children,
-                          const AbstractExpression *predicate, const AbstractExpression *inner_predicate,
-                          table_oid_t inner_table_oid, std::string index_name)
+                          const AbstractExpression *predicate, table_oid_t inner_table_oid, std::string index_name,
+                          const Schema *outer_table_schema, const Schema *inner_table_schema)
       : AbstractPlanNode(output_schema, std::move(children)),
         predicate_(predicate),
-        inner_predicate_(inner_predicate),
         inner_table_oid_(inner_table_oid),
-        index_name_(std::move(index_name)) {}
+        index_name_(std::move(index_name)),
+        outer_table_schema_(outer_table_schema),
+        inner_table_schema_(inner_table_schema) {}
 
   PlanType GetType() const override { return PlanType::NestedIndexJoin; }
 
   /** @return the predicate to be used in the nested index join */
   const AbstractExpression *Predicate() const { return predicate_; }
 
-  /** @return the predicate for the inner table to be used in the nested index join */
-  const AbstractExpression *Inner_Predicate() const { return inner_predicate_; }
-
   /** @return the plan node for the outer table of the nested index join */
   const AbstractPlanNode *GetChildPlan() const { return GetChildAt(0); }
 
+  /** @return the table oid for the inner table of the nested index join */
   table_oid_t GetInnerTableOid() const { return inner_table_oid_; }
 
-  const std::string &GetIndexName() const { return index_name_; }
+  /** @return the index associated with the nested index join */
+  std::string GetIndexName() const { return index_name_; }
+
+  /** @return Schema with needed columns in from the outer table */
+  const Schema *OuterTableSchema() const { return outer_table_schema_; }
+
+  /** @return Schema with needed columns in from the inner table */
+  const Schema *InnerTableSchema() const { return inner_table_schema_; }
 
  private:
   /** The nested index join predicate. */
   const AbstractExpression *predicate_;
-  const AbstractExpression *inner_predicate_;
   table_oid_t inner_table_oid_;
   const std::string index_name_;
+  const Schema *outer_table_schema_;
+  const Schema *inner_table_schema_;
 };
 }  // namespace bustub
