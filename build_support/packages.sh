a145c9c06bc28670dcbed3595b86494a5899f75e
kriti-sc <kathuriakriti1@gmail.com>
2021-12-11 20:32:56 +0530

    Install LLVM version that has Apple Silicon support
    LLVM 8 does not have Apple Silicon support: https://formulae.brew.sh/formula/llvm@8

--- build_support/packages.sh
+++ build_support/packages.sh
@@ -76,7 +76,7 @@ install_mac() {
   brew ls --versions coreutils || brew install coreutils
   brew ls --versions doxygen || brew install doxygen
   brew ls --versions git || brew install git
-  (brew ls --versions llvm | grep 8) || brew install llvm@8
+  (brew ls --versions llvm | grep 12) || brew install llvm@12
 }
 
 install_linux() {
