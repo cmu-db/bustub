77be19ecee460e8051f20c8ad89e19677a159bcd
Garrison Hess <garrison.hess@gmail.com>
2022-05-25 16:56:18 -0400

    Fix typo again

--- src/include/storage/page/b_plus_tree_internal_page.h
+++ src/include/storage/page/b_plus_tree_internal_page.h
@@ -62,6 +62,6 @@ class BPlusTreeInternalPage : public BPlusTreePage {
   void CopyLastFrom(const MappingType &pair, BufferPoolManager *buffer_pool_manager);
   void CopyFirstFrom(const MappingType &pair, BufferPoolManager *buffer_pool_manager);
   // Flexible array member for page data.
-  MappingType array[1];
+  MappingType array_[1];
 };
 }  // namespace bustub
