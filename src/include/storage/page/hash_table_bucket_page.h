88f0311f5249397970435402e849af626931a86b
Isaac <nasnoisaac@gmail.com>
2022-08-15 11:42:10 +0800

    Add missing comment for flexible array member (#253)

--- src/include/storage/page/hash_table_bucket_page.h
+++ src/include/storage/page/hash_table_bucket_page.h
@@ -142,6 +142,7 @@ class HashTableBucketPage {
   char occupied_[(BUCKET_ARRAY_SIZE - 1) / 8 + 1];
   // 0 if tombstone/brand new (never occupied), 1 otherwise.
   char readable_[(BUCKET_ARRAY_SIZE - 1) / 8 + 1];
+  // Flexible array member for page data.
   MappingType array_[1];
 };
 
