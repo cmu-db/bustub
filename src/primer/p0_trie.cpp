7307f94617eda60c175c6e113288e16953536f96
Alex Chi <iskyzh@gmail.com>
2022-08-27 18:54:28 -0400

    feat(build): add check-clang-tidy-p0 for p0 (#289)
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

---
+++ src/primer/p0_trie.cpp
@@ -0,0 +1,9 @@
+#include "primer/p0_trie.h"
+
+// This is a placeholder file for clang-tidy check.
+//
+// With this file, we can fire run_clang_tidy.py to check `p0_trie.h`,
+// as it will filter out all header files and won't check header-only code.
+//
+// This file is not part of the submission. All of the modifications should
+// be done in `src/include/primer/p0_trie.h`.
