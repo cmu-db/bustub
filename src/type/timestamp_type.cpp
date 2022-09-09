ba347add711144305095a20e675c3ae577362693
Peter Berger <pberger@apple.com>
2022-09-09 11:17:18 -0400

    Addressing clang-check-tidy identifier naming concerns.

--- src/type/timestamp_type.cpp
+++ src/type/timestamp_type.cpp
@@ -114,11 +114,11 @@ auto TimestampType::ToString(const Value &val) const -> std::string {
   auto day = static_cast<uint16_t>(tm % 32);
   tm /= 32;
   auto month = static_cast<uint16_t>(tm);
-  const size_t dateStrLen = 30;
-  const size_t zoneLen = 5;
-  char str[dateStrLen];
-  char zone[zoneLen];
-  snprintf(str, dateStrLen, "%04d-%02d-%02d %02d:%02d:%02d.%06d", year, month, day, hour, min, sec, micro);
+  const size_t date_str_len = 30;
+  const size_t zone_len = 5;
+  char str[date_str_len];
+  char zone[zone_len];
+  snprintf(str, date_str_len, "%04d-%02d-%02d %02d:%02d:%02d.%06d", year, month, day, hour, min, sec, micro);
   if (tz >= 0) {
     str[26] = '+';
   } else {
@@ -127,7 +127,7 @@ auto TimestampType::ToString(const Value &val) const -> std::string {
   if (tz < 0) {
     tz = -tz;
   }
-  snprintf(zone, zoneLen, "%02d", tz);  // NOLINT
+  snprintf(zone, zone_len, "%02d", tz);  // NOLINT
   str[27] = 0;
   return std::string(std::string(str) + std::string(zone));
 }
