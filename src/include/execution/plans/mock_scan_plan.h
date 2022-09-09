a5131ae63ed4d464e10174bd206203aa22325fe7
Alex Chi <iskyzh@gmail.com>
2022-09-09 16:59:37 -0400

    feat(executor): add contents for mock table
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/include/execution/plans/mock_scan_plan.h
+++ src/include/execution/plans/mock_scan_plan.h
@@ -13,6 +13,7 @@
 #pragma once
 
 #include <string>
+#include <utility>
 
 #include "catalog/catalog.h"
 #include "execution/expressions/abstract_expression.h"
@@ -34,6 +35,15 @@ class MockScanPlanNode : public AbstractPlanNode {
    */
   MockScanPlanNode(const Schema *output, std::size_t size) : AbstractPlanNode(output, {}), size_{size} {}
 
+  /**
+   * Construct a new MockScanPlanNode instance.
+   * @param output The output schema of this mock scan plan node
+   * @param size The size of the scanned table
+   * @param table The table name of this mock scan node, used to determine the generated content.
+   */
+  MockScanPlanNode(const Schema *output, std::size_t size, std::string table)
+      : AbstractPlanNode(output, {}), size_{size}, table_(std::move(table)) {}
+
   /** @return The type of the plan node */
   auto GetType() const -> PlanType override { return PlanType::MockScan; }
 
@@ -46,6 +56,9 @@ class MockScanPlanNode : public AbstractPlanNode {
   /** Increment the poll count for this mock scan */
   void IncrementPollCount() const { poll_count_++; }
 
+  /** @return The table name of this mock scan node, used to determine the generated content. */
+  auto GetTable() const -> const std::string & { return table_; }
+
  protected:
   auto PlanNodeToString() const -> std::string override { return fmt::format("MockScan {{ size={} }}", size_); }
 
@@ -54,6 +67,8 @@ class MockScanPlanNode : public AbstractPlanNode {
   const std::size_t size_;
   /** The total number of times the executor for this plan is polled */
   mutable std::size_t poll_count_{0};
+  /** The table name of this mock scan executor */
+  std::string table_;
 };
 
 }  // namespace bustub
