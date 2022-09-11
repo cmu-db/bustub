38840cfe4b432f4574c7ef8a52adb05b35aa4475
Alex Chi <iskyzh@gmail.com>
2022-09-10 23:43:03 -0400

    remove distinct executor
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- test/execution/executor_test.cpp
+++ test/execution/executor_test.cpp
@@ -29,7 +29,6 @@
 #include "execution/expressions/comparison_expression.h"
 #include "execution/expressions/constant_value_expression.h"
 #include "execution/plans/delete_plan.h"
-#include "execution/plans/distinct_plan.h"
 #include "execution/plans/hash_join_plan.h"
 #include "execution/plans/limit_plan.h"
 #include "execution/plans/seq_scan_plan.h"
@@ -619,42 +618,6 @@ TEST_F(ExecutorTest, DISABLED_SimpleLimitTest) {
     ASSERT_EQ(tuple.GetValue(out_schema, out_schema->GetColIdx("colA")).GetAs<int32_t>(), static_cast<int32_t>(i));
     ASSERT_EQ(tuple.GetValue(out_schema, out_schema->GetColIdx("colB")).GetAs<int32_t>(), static_cast<int32_t>(i));
   }
-}
-
-// SELECT DISTINCT colC FROM test_7
-TEST_F(ExecutorTest, DISABLED_SimpleDistinctTest) {
-  auto *table_info = GetExecutorContext()->GetCatalog()->GetTable("test_7");
-  auto &schema = table_info->schema_;
-
-  auto *col_c = MakeColumnValueExpression(schema, 0, "colC");
-  auto *out_schema = MakeOutputSchema({{"colC", col_c}});
-
-  // Construct sequential scan
-  auto seq_scan_plan = std::make_unique<SeqScanPlanNode>(out_schema, nullptr, table_info->oid_);
-
-  // Construct the distinct plan
-  auto distinct_plan = std::make_unique<DistinctPlanNode>(out_schema, seq_scan_plan.get());
-
-  // Execute sequential scan with DISTINCT
-  std::vector<Tuple> result_set{};
-  GetExecutionEngine()->Execute(distinct_plan.get(), &result_set, GetTxn(), GetExecutorContext());
-
-  // Verify results; colC is cyclic on 0 - 9
-  ASSERT_EQ(result_set.size(), 10);
-
-  // Results are unordered
-  std::vector<int32_t> results{};
-  results.reserve(result_set.size());
-  std::transform(result_set.cbegin(), result_set.cend(), std::back_inserter(results), [=](const Tuple &tuple) {
-    return tuple.GetValue(out_schema, out_schema->GetColIdx("colC")).GetAs<int32_t>();
-  });
-  std::sort(results.begin(), results.end());
-
-  // Expect keys 0 - 9
-  std::vector<int32_t> expected(result_set.size());
-  std::iota(expected.begin(), expected.end(), 0);
-
-  ASSERT_TRUE(std::equal(results.cbegin(), results.cend(), expected.cbegin()));
 }
 
 }  // namespace bustub
