fd0db6ca2502e9cb2079ff7ee41021b43913d2dc
Alex Chi <iskyzh@gmail.com>
2022-09-10 22:03:48 -0400

    feat(optimizer): add noop optimizer
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

---
+++ src/include/optimizer/optimizer.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "catalog/catalog.h"
+#include "execution/plans/abstract_plan.h"
+
+namespace bustub {
+
+/**
+ * The optimizer takes an `AbstractPlanNode` and outputs an optimized `AbstractPlanNode`.
+ */
+class Optimizer {
+ public:
+  explicit Optimizer(const Catalog &catalog) : catalog_(catalog) {}
+
+  auto Optimize(AbstractPlanNode *plan) -> AbstractPlanNode *;
+
+ private:
+  /** Catalog will be used during the planning process. USERS SHOULD ENSURE IT OUTLIVES
+   * OPTIMIZER, otherwise it's a dangling reference.
+   */
+  [[maybe_unused]] const Catalog &catalog_;
+};
+
+}  // namespace bustub
---
+++ src/optimizer/optimizer.cpp
@@ -0,0 +1,5 @@
+#include "optimizer/optimizer.h"
+
+namespace bustub {
+auto Optimizer::Optimize(AbstractPlanNode *plan) -> AbstractPlanNode * { return plan; }
+}  // namespace bustub
