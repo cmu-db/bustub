c883c626271a6bb48799a6d76e65c34fc521ca1c
Alex Chi <iskyzh@gmail.com>
2022-09-16 20:04:42 -0400

    move indent to string utils
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/binder/statement/insert_statement.cpp
+++ src/binder/statement/insert_statement.cpp
@@ -8,22 +8,12 @@
 
 namespace bustub {
 
-auto IndentAllLines(const std::string &lines, size_t num_indent) -> std::string {
-  std::vector<std::string> lines_str;
-  auto lines_split = StringUtil::Split(lines, '\n');
-  lines_str.reserve(lines_split.size());
-  auto indent_str = StringUtil::Indent(num_indent);
-  for (auto &line : lines_split) {
-    lines_str.push_back(fmt::format("{}{}", indent_str, line));
-  }
-  return fmt::format("\n{}", fmt::join(lines_str, "\n"));
-}
-
 InsertStatement::InsertStatement(std::string table, std::unique_ptr<SelectStatement> select)
     : BoundStatement(StatementType::INSERT_STATEMENT), table_(std::move(table)), select_(std::move(select)) {}
 
 auto InsertStatement::ToString() const -> std::string {
-  return fmt::format("BoundInsert {{\n  table={},\n  select={}\n}}", table_, IndentAllLines(select_->ToString(), 2));
+  return fmt::format("BoundInsert {{\n  table={},\n  select={}\n}}", table_,
+                     StringUtil::IndentAllLines(select_->ToString(), 2));
 }
 
 }  // namespace bustub
