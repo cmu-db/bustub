fe9bda39c417aa5a12628842a802eaaffacbfc0a
Alex Chi <iskyzh@gmail.com>
2022-09-16 21:24:59 -0400

    chore(bpm): throw when unimplemented (#365)
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/buffer/buffer_pool_manager_instance.cpp
+++ src/buffer/buffer_pool_manager_instance.cpp
@@ -12,6 +12,7 @@
 
 #include "buffer/buffer_pool_manager_instance.h"
 
+#include "common/exception.h"
 #include "common/macros.h"
 
 namespace bustub {
@@ -28,6 +29,11 @@ BufferPoolManagerInstance::BufferPoolManagerInstance(size_t pool_size, DiskManag
   for (size_t i = 0; i < pool_size_; ++i) {
     free_list_.emplace_back(static_cast<int>(i));
   }
+
+  // TODO(students): remove this line after you have implemented the buffer pool manager
+  throw NotImplementedException(
+      "BufferPoolManager is not implemented yet. If you have finished implementing BPM, please remove the throw "
+      "exception line in `buffer_pool_manager_instance.cpp`.");
 }
 
 BufferPoolManagerInstance::~BufferPoolManagerInstance() {
