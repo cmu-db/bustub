7caabce765c1916a96d827f068c96f11f1a8a731
Alex Chi <iskyzh@gmail.com>
2022-09-16 20:07:55 -0400

    feat(planner): plan insert (#340)
    * feat(planner): plan insert
    
    Signed-off-by: Alex Chi <iskyzh@gmail.com>
    
    * implement values executor
    
    Signed-off-by: Alex Chi <iskyzh@gmail.com>
    
    * commit txn, ignore null schema, fix table planning bug
    
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

---
+++ src/planner/plan_insert.cpp
@@ -0,0 +1,19 @@
+#include <memory>
+
+#include "binder/statement/insert_statement.h"
+#include "execution/expressions/column_value_expression.h"
+#include "execution/plans/insert_plan.h"
+#include "planner/planner.h"
+
+namespace bustub {
+
+auto Planner::PlanInsert(const InsertStatement &statement) -> std::unique_ptr<AbstractPlanNode> {
+  auto select = PlanSelect(*statement.select_);
+
+  auto table = catalog_.GetTable(statement.table_);
+  BUSTUB_ASSERT(table, "table not found");
+
+  return std::make_unique<InsertPlanNode>(SavePlanNode(std::move(select)), table->oid_);
+}
+
+}  // namespace bustub
