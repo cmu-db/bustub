46ba44e443d7b5a2723af89c06ecbf9c847ae426
Alex Chi <iskyzh@gmail.com>
2022-09-19 20:19:05 -0400

    fix(planner): check insert schema match
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/planner/plan_insert.cpp
+++ src/planner/plan_insert.cpp
@@ -1,7 +1,9 @@
+#include <algorithm>
 #include <memory>
 
 #include "binder/bound_expression.h"
 #include "binder/statement/insert_statement.h"
+#include "common/exception.h"
 #include "execution/expressions/column_value_expression.h"
 #include "execution/plans/insert_plan.h"
 #include "planner/planner.h"
@@ -13,6 +15,13 @@ auto Planner::PlanInsert(const InsertStatement &statement) -> AbstractPlanNodeRe
 
   auto table = catalog_.GetTable(statement.table_);
   BUSTUB_ASSERT(table, "table not found");
+
+  const auto &table_schema = table->schema_.GetColumns();
+  const auto &child_schema = select->OutputSchema().GetColumns();
+  if (!std::equal(table_schema.cbegin(), table_schema.cend(), child_schema.cbegin(), child_schema.cend(),
+                  [](auto &&col1, auto &&col2) { return col1.GetType() == col2.GetType(); })) {
+    throw bustub::Exception("table schema mismatch");
+  }
 
   return std::make_shared<InsertPlanNode>(std::move(select), table->oid_);
 }
