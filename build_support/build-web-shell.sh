f134f54f728d1fe0d76eef19f611027bbf7eca53
Alex Chi <iskyzh@gmail.com>
2022-10-07 16:31:03 -0400

    feat(tools): add btree shell
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- build_support/build-web-shell.sh
+++ build_support/build-web-shell.sh
@@ -11,9 +11,22 @@ BUSTUB_PUBLIC_VERSION="$(git -C "${DIR}/../../bustub_public" describe --always -
 mkdir -p cmake-build-wasm
 cd cmake-build-wasm
 emcmake cmake .. -DCMAKE_BUILD_TYPE=MinSizeRel
-make -j$(nproc) wasm-shell
-cp -a ../tools/wasm-shell/extra_files/* bin/
-sed -i '' "s|\${BUSTUB_PRIVATE_VERSION}|${BUSTUB_PRIVATE_VERSION}|" bin/index.html
-sed -i '' "s|\${BUSTUB_PUBLIC_VERSION}|${BUSTUB_PUBLIC_VERSION}|" bin/index.html
 
-ls -alh bin/
+make -j$(nproc) wasm-shell
+mkdir -p wasm-shell
+cp bin/bustub-wasm-shell.js wasm-shell
+cp bin/bustub-wasm-shell.wasm wasm-shell
+cp -a ../tools/wasm-shell/extra_files/* wasm-shell/
+sed -i '' "s|\${BUSTUB_PRIVATE_VERSION}|${BUSTUB_PRIVATE_VERSION}|" wasm-shell/index.html
+sed -i '' "s|\${BUSTUB_PUBLIC_VERSION}|${BUSTUB_PUBLIC_VERSION}|" wasm-shell/index.html
+
+make -j$(nproc) wasm-bpt-printer
+mkdir -p wasm-bpt-printer
+cp bin/bustub-wasm-bpt-printer.js wasm-bpt-printer
+cp bin/bustub-wasm-bpt-printer.wasm wasm-bpt-printer
+cp -a ../tools/wasm-bpt-printer/extra_files/* wasm-bpt-printer/
+sed -i '' "s|\${BUSTUB_PRIVATE_VERSION}|${BUSTUB_PRIVATE_VERSION}|" wasm-bpt-printer/index.html
+sed -i '' "s|\${BUSTUB_PUBLIC_VERSION}|${BUSTUB_PUBLIC_VERSION}|" wasm-bpt-printer/index.html
+
+ls -alh wasm-shell/
+ls -alh wasm-bpt-printer/
---
+++ tools/wasm-bpt-printer/wasm-bpt-printer.cpp
@@ -0,0 +1,128 @@
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <string>
+#include "binder/binder.h"
+#include "buffer/buffer_pool_manager_instance.h"
+#include "common/bustub_instance.h"
+#include "common/exception.h"
+#include "common/logger.h"
+#include "common/util/string_util.h"
+#include "concurrency/transaction.h"
+#include "linenoise/linenoise.h"
+#include "storage/index/b_plus_tree.h"
+#include "test_util.h"  // NOLINT
+#include "utf8proc/utf8proc.h"
+
+using bustub::BPlusTree;
+using bustub::BufferPoolManager;
+using bustub::BufferPoolManagerInstance;
+using bustub::DiskManager;
+using bustub::Exception;
+using bustub::GenericComparator;
+using bustub::GenericKey;
+using bustub::page_id_t;
+using bustub::ParseCreateStatement;
+using bustub::RID;
+using bustub::Transaction;
+
+auto UsageMessage() -> std::string {
+  std::string message =
+      "Enter any of the following commands after the prompt > :\n"
+      "\ti <k>  -- Insert <k> (int64_t) as both key and value).\n"
+      "\td <k>  -- Delete key <k> and its associated value.\n"
+      "\t? -- Print this help message.";
+  return message;
+}
+
+using BPT = BPlusTree<GenericKey<8>, RID, GenericComparator<8>>;
+BPT *tree = nullptr;
+BufferPoolManager *bpm = nullptr;
+Transaction *transaction = nullptr;
+std::unique_ptr<bustub::Schema> key_schema = nullptr;
+
+extern "C" {
+
+auto BustubInit(int leaf_max_size, int internal_max_size) -> int {
+  // create KeyComparator and index schema
+  std::string create_stmt = "a bigint";
+  try {
+    key_schema = ParseCreateStatement(create_stmt);
+  } catch (Exception &ex) {
+    std::cerr << "Failed to parse create statement: " << ex.what() << std::endl;
+  }
+
+  GenericComparator<8> comparator(key_schema.get());
+
+  auto *disk_manager = new DiskManager("test.db");
+  bpm = new BufferPoolManagerInstance(100, disk_manager);
+  // create header_page
+  page_id_t page_id;
+  bpm->NewPage(&page_id);
+  // create b+ tree
+  tree = new BPT("foo_pk", bpm, comparator, leaf_max_size, internal_max_size);
+  transaction = new Transaction(0);
+  return 0;
+}
+
+auto BustubApplyCommand(const char *input, char *output, uint16_t len) -> int {
+  std::cout << "Apply!" << std::endl;
+
+  GenericKey<8> index_key;
+  int64_t key = 0;
+  RID rid;
+  std::string output_string;
+  std::stringstream ss(input);
+  char instruction;
+  ss >> instruction;
+  if (!ss) {
+    return 1;
+  }
+
+  switch (instruction) {
+    case 'd':
+      std::cout << "delete " << key << std::endl;
+      ss >> key;
+      if (!ss) {
+        return 1;
+      }
+      index_key.SetFromInteger(key);
+      tree->Remove(index_key, transaction);
+      break;
+    case 'i':
+      ss >> key;
+      if (!ss) {
+        return 1;
+      }
+      std::cout << "insert " << key << std::endl;
+      rid.Set(static_cast<int32_t>(key >> 32), static_cast<int>(key & 0xFFFFFFFF));
+      index_key.SetFromInteger(key);
+      tree->Insert(index_key, rid, transaction);
+      break;
+    case '?':
+      std::cout << UsageMessage();
+      break;
+  }
+
+  tree->Draw(bpm, "test.out");
+  std::ifstream x("test.out");
+  while (x) {
+    std::string str;
+    std::getline(x, str);
+    output_string += str;
+  }
+  x.close();
+  remove("test.out");
+
+  strncpy(output, output_string.c_str(), len);
+  if (output_string.length() >= len) {
+    return 2;
+  }
+
+  return 0;
+}
+}
