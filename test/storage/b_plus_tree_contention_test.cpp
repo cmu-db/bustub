ad1eda16ac674212b1f05e86775e2d5eea5edf8e
Alex Chi <iskyzh@gmail.com>
2022-11-02 13:21:32 -0400

    fix
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- test/storage/b_plus_tree_contention_test.cpp
+++ test/storage/b_plus_tree_contention_test.cpp
@@ -74,7 +74,7 @@ bool BPlusTreeLockBenchmarkCall(size_t num_threads, int leaf_node_size, bool wit
   return success;
 }
 
-TEST(BPlusTreeTest, BPlusTreeContentionBenchmark) {  // NOLINT
+TEST(BPlusTreeTest, DISABLED_BPlusTreeContentionBenchmark) {  // NOLINT
   std::vector<size_t> time_ms_with_mutex;
   std::vector<size_t> time_ms_wo_mutex;
   for (size_t iter = 0; iter < 20; iter++) {
@@ -114,7 +114,7 @@ TEST(BPlusTreeTest, BPlusTreeContentionBenchmark) {  // NOLINT
             << std::endl;
 }
 
-TEST(BPlusTreeTest, BPlusTreeContentionBenchmark2) {  // NOLINT
+TEST(BPlusTreeTest, DISABLED_BPlusTreeContentionBenchmark2) {  // NOLINT
   std::vector<size_t> time_ms_with_mutex;
   std::vector<size_t> time_ms_wo_mutex;
   for (size_t iter = 0; iter < 20; iter++) {
