a366c733a09e13baa93b7618b9ebc904ca553804
Alex Chi <iskyzh@gmail.com>
2022-11-02 11:53:16 -0400

    fix
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/execution/update_executor.cpp
+++ src/execution/update_executor.cpp
@@ -23,28 +23,4 @@ void UpdateExecutor::Init() { throw NotImplementedException("UpdateExecutor is n
 
 auto UpdateExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool { return false; }
 
-auto UpdateExecutor::GenerateUpdatedTuple(const Tuple &src_tuple) -> Tuple {
-  const auto &update_attrs = plan_->GetUpdateAttr();
-  Schema schema = table_info_->schema_;
-  uint32_t col_count = schema.GetColumnCount();
-  std::vector<Value> values;
-  for (uint32_t idx = 0; idx < col_count; idx++) {
-    if (update_attrs.find(idx) == update_attrs.cend()) {
-      values.emplace_back(src_tuple.GetValue(&schema, idx));
-    } else {
-      const UpdateInfo info = update_attrs.at(idx);
-      Value val = src_tuple.GetValue(&schema, idx);
-      switch (info.type_) {
-        case UpdateType::Add:
-          values.emplace_back(val.Add(ValueFactory::GetIntegerValue(info.update_val_)));
-          break;
-        case UpdateType::Set:
-          values.emplace_back(ValueFactory::GetIntegerValue(info.update_val_));
-          break;
-      }
-    }
-  }
-  return Tuple{values, &schema};
-}
-
 }  // namespace bustub
