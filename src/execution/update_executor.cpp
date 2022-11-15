696f4c0afb6dd6f031a03472522eac4463b1c0bb
Alex Chi <iskyzh@gmail.com>
2022-11-15 10:43:25 -0500

    feat(p4): CMU terrier benchmark
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/execution/update_executor.cpp
+++ src/execution/update_executor.cpp
@@ -17,7 +17,9 @@ namespace bustub {
 
 UpdateExecutor::UpdateExecutor(ExecutorContext *exec_ctx, const UpdatePlanNode *plan,
                                std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx) {
+  // As of Fall 2022, you DON'T need to implement update executor to have perfect score in project 3 / project 4.
+}
 
 void UpdateExecutor::Init() { throw NotImplementedException("UpdateExecutor is not implemented"); }
 
