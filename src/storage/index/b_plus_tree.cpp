c3dfff2dd6895897aa5bc84784cae454ab94a048
Abdallah Ahmed <40672207+xUser5000@users.noreply.github.com>
2023-01-05 21:19:16 +0200

    fix(typos): b_plus_tree.cpp routine descriptions

--- src/storage/index/b_plus_tree.cpp
+++ src/storage/index/b_plus_tree.cpp
@@ -67,7 +67,7 @@ void BPLUSTREE_TYPE::Remove(const KeyType &key, Transaction *transaction) {}
  * INDEX ITERATOR
  *****************************************************************************/
 /*
- * Input parameter is void, find the leaftmost leaf page first, then construct
+ * Input parameter is void, find the leftmost leaf page first, then construct
  * index iterator
  * @return : index iterator
  */
@@ -103,7 +103,7 @@ auto BPLUSTREE_TYPE::GetRootPageId() -> page_id_t { return 0; }
  * Update/Insert root page id in header page(where page_id = 0, header_page is
  * defined under include/page/header_page.h)
  * Call this method everytime root page id is changed.
- * @parameter: insert_record      defualt value is false. When set to true,
+ * @parameter: insert_record      default value is false. When set to true,
  * insert a record <index_name, root_page_id> into header page instead of
  * updating it.
  */
