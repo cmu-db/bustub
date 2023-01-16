38ebd2c431f2a2c09eafe75858de7524967616e5
Alex Chi <iskyzh@gmail.com>
2023-01-16 18:24:48 -0500

    move files
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- test/primer/trie_store_noncopy_test.cpp
+++ test/primer/trie_store_noncopy_test.cpp
@@ -15,7 +15,7 @@ namespace bustub {
 
 using Integer = std::unique_ptr<uint32_t>;
 
-TEST(TrieTest, NonCopyableTest) {
+TEST(TrieStoreTest, NonCopyableTest) {
   auto store = TrieStore();
   store.Put<Integer>("tes", std::make_unique<uint32_t>(233));
   store.Put<Integer>("te", std::make_unique<uint32_t>(23));
@@ -29,6 +29,46 @@ TEST(TrieTest, NonCopyableTest) {
   ASSERT_EQ(store.Get<Integer>("te"), std::nullopt);
   ASSERT_EQ(store.Get<Integer>("tes"), std::nullopt);
   ASSERT_EQ(store.Get<Integer>("test"), std::nullopt);
+}
+
+TEST(TrieStoreTest, ReadWriteTest) {
+  auto store = TrieStore();
+  store.Put<uint32_t>("a", 1);
+  store.Put<uint32_t>("b", 2);
+  store.Put<uint32_t>("c", 3);
+  std::promise<int> x;
+
+  std::cerr << "[0] begin" << std::endl;
+
+  std::thread t([&store, &x] { store.Put<MoveBlocked>("d", MoveBlocked(x.get_future())); });
+
+  std::cerr << "[1] thread spawn" << std::endl;
+
+  // Loop for enough time to ensure that the thread is blocked on the promise.
+  for (int i = 0; i < 100000; i++) {
+    {
+      auto guard = store.Get<uint32_t>("a");
+      ASSERT_EQ(**guard, 1);
+    }
+    {
+      auto guard = store.Get<uint32_t>("b");
+      ASSERT_EQ(**guard, 2);
+    }
+    {
+      auto guard = store.Get<uint32_t>("c");
+      ASSERT_EQ(**guard, 3);
+    }
+  }
+
+  std::cerr << "[2] read done" << std::endl;
+
+  x.set_value(233);
+
+  t.join();
+
+  std::cerr << "[3] write complete" << std::endl;
+
+  ASSERT_NE(store.Get<MoveBlocked>("d"), std::nullopt);
 }
 
 }  // namespace bustub
--- test/primer/trie_store_test.cpp
+++ test/primer/trie_store_test.cpp
@@ -81,46 +81,6 @@ TEST(TrieStoreTest, MixedTest) {
   }
 }
 
-TEST(TrieStoreTest, ReadWriteTest) {
-  auto store = TrieStore();
-  store.Put<uint32_t>("a", 1);
-  store.Put<uint32_t>("b", 2);
-  store.Put<uint32_t>("c", 3);
-  std::promise<int> x;
-
-  std::cerr << "[0] begin" << std::endl;
-
-  std::thread t([&store, &x] { store.Put<MoveBlocked>("d", MoveBlocked(x.get_future())); });
-
-  std::cerr << "[1] thread spawn" << std::endl;
-
-  // Loop for enough time to ensure that the thread is blocked on the promise.
-  for (int i = 0; i < 100000; i++) {
-    {
-      auto guard = store.Get<uint32_t>("a");
-      ASSERT_EQ(**guard, 1);
-    }
-    {
-      auto guard = store.Get<uint32_t>("b");
-      ASSERT_EQ(**guard, 2);
-    }
-    {
-      auto guard = store.Get<uint32_t>("c");
-      ASSERT_EQ(**guard, 3);
-    }
-  }
-
-  std::cerr << "[2] read done" << std::endl;
-
-  x.set_value(233);
-
-  t.join();
-
-  std::cerr << "[3] write complete" << std::endl;
-
-  ASSERT_NE(store.Get<MoveBlocked>("d"), std::nullopt);
-}
-
 TEST(TrieStoreTest, MixedConcurrentTest) {
   auto store = TrieStore();
 
