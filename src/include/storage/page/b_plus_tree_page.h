0870bfea9dfa9e67a4f57b68f66ee1c25e5a81e3
Alex Chi <iskyzh@gmail.com>
2023-02-20 16:23:50 -0500

    fix: remove HeaderPage to avoid confusion (#511)
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/include/storage/page/b_plus_tree_page.h
+++ src/include/storage/page/b_plus_tree_page.h
@@ -56,12 +56,6 @@ class BPlusTreePage {
   void SetMaxSize(int max_size);
   auto GetMinSize() const -> int;
 
-  auto GetParentPageId() const -> page_id_t;
-  void SetParentPageId(page_id_t parent_page_id);
-
-  auto GetPageId() const -> page_id_t;
-  void SetPageId(page_id_t page_id);
-
  private:
   // member variable, attributes that both internal and leaf page share
   IndexPageType page_type_ __attribute__((__unused__));
