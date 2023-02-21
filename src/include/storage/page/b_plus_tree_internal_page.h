40f091d508b0fbe7993030e44a5cc67259733c14
Alex Chi <iskyzh@gmail.com>
2023-02-21 14:05:19 -0500

    fix page size
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/include/storage/page/b_plus_tree_internal_page.h
+++ src/include/storage/page/b_plus_tree_internal_page.h
@@ -17,7 +17,7 @@
 namespace bustub {
 
 #define B_PLUS_TREE_INTERNAL_PAGE_TYPE BPlusTreeInternalPage<KeyType, ValueType, KeyComparator>
-#define INTERNAL_PAGE_HEADER_SIZE 24
+#define INTERNAL_PAGE_HEADER_SIZE 12
 #define INTERNAL_PAGE_SIZE ((BUSTUB_PAGE_SIZE - INTERNAL_PAGE_HEADER_SIZE) / (sizeof(MappingType)))
 /**
  * Store n indexed keys and n+1 child pointers (page_id) within internal page.
