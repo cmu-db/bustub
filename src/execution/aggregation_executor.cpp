83efc2a8c4d422017f00952ddf36b004e453098f
Hiki <aneureka2@gmail.com>
2023-06-21 08:54:04 +0800

    style: rename child -> child_executor to keep consistency (#575)

--- src/execution/aggregation_executor.cpp
+++ src/execution/aggregation_executor.cpp
@@ -17,13 +17,13 @@
 namespace bustub {
 
 AggregationExecutor::AggregationExecutor(ExecutorContext *exec_ctx, const AggregationPlanNode *plan,
-                                         std::unique_ptr<AbstractExecutor> &&child)
+                                         std::unique_ptr<AbstractExecutor> &&child_executor)
     : AbstractExecutor(exec_ctx) {}
 
 void AggregationExecutor::Init() {}
 
 auto AggregationExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
 
-auto AggregationExecutor::GetChildExecutor() const -> const AbstractExecutor * { return child_.get(); }
+auto AggregationExecutor::GetChildExecutor() const -> const AbstractExecutor * { return child_executor_.get(); }
 
 }  // namespace bustub
