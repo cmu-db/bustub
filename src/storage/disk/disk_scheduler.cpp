975636b6143d80d3fe93e6c1f08c7d4e43361d3e
fernandolis10 <0fernando.lisboa@gmail.com>
2023-09-02 19:43:05 -0400

    Fixed formatting.

--- src/storage/disk/disk_scheduler.cpp
+++ src/storage/disk/disk_scheduler.cpp
@@ -19,8 +19,8 @@ namespace bustub {
 DiskScheduler::DiskScheduler(DiskManager *disk_manager) : disk_manager_(disk_manager) {
   // TODO(P1): remove this line after you have implemented the disk scheduler API
   throw NotImplementedException(
-      "DiskScheduler is not implemented yet. If you have finished implementing the disk scheduler, please remove the throw "
-      "exception line in `disk_scheduler.cpp`.");
+      "DiskScheduler is not implemented yet. If you have finished implementing the disk scheduler, please remove the "
+      "throw exception line in `disk_scheduler.cpp`.");
 
   // Spawn the background thread
   background_thread_.emplace([&] { StartWorkerThread(); });
