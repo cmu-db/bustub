cd34aec1cd63e7fd52fe47d01c62e8f091bd9c47
Avery <averyqi115@gmail.com>
2023-10-30 19:34:01 -0400

    update project-3.yaml; add leaderboard for q1 window (#641)
    * update project-3.yaml; add leaderboard for q1 window; fix window function refsol
    
    Signed-off-by: AveryQi115 <averyqi115@gmail.com>
    
    * change leaderboard q3.1
    
    Signed-off-by: AveryQi115 <averyqi115@gmail.com>
    
    * update leaderboard q3.1
    
    Signed-off-by: AveryQi115 <averyqi115@gmail.com>
    
    ---------
    
    Signed-off-by: AveryQi115 <averyqi115@gmail.com>

--- src/execution/mock_scan_executor.cpp
+++ src/execution/mock_scan_executor.cpp
@@ -53,7 +53,7 @@ const char *mock_table_list[] = {"__mock_table_1", "__mock_table_2", "__mock_tab
                                  // For leaderboard Q2
                                  "__mock_t4_1m", "__mock_t5_1m", "__mock_t6_1m",
                                  // For leaderboard Q3
-                                 "__mock_t7", "__mock_t8", nullptr};
+                                 "__mock_t7", "__mock_t8", "__mock_t9", nullptr};
 
 static const int GRAPH_NODE_CNT = 10;
 
@@ -124,6 +124,10 @@ auto GetMockTableSchemaOf(const std::string &table) -> Schema {
     return Schema{std::vector{Column{"v4", TypeId::INTEGER}}};
   }
 
+  if (table == "__mock_t9") {
+    return Schema{std::vector{Column{"x", TypeId::INTEGER}, Column{"y", TypeId::INTEGER}}};
+  }
+
   throw bustub::Exception(fmt::format("mock table {} not found", table));
 }
 
@@ -192,6 +196,10 @@ auto GetSizeOf(const MockScanPlanNode *plan) -> size_t {
 
   if (table == "__mock_t8") {
     return 10;
+  }
+
+  if (table == "__mock_t9") {
+    return 1000000;
   }
 
   return 0;
@@ -406,6 +414,15 @@ auto GetFunctionOf(const MockScanPlanNode *plan) -> std::function<Tuple(size_t)>
     return [plan](size_t cursor) {
       std::vector<Value> values{};
       values.push_back(ValueFactory::GetIntegerValue(cursor));
+      return Tuple{values, &plan->OutputSchema()};
+    };
+  }
+
+  if (table == "__mock_t9") {
+    return [plan](size_t cursor) {
+      std::vector<Value> values{};
+      values.push_back(ValueFactory::GetIntegerValue(cursor / 10000));
+      values.push_back(ValueFactory::GetIntegerValue(cursor / 2 + ((cursor / 10000) % 2) * ((cursor / 2) % 2)));
       return Tuple{values, &plan->OutputSchema()};
     };
   }
