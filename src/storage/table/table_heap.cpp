bd470805b88d5a11803b8e0651b340452a3ac7d5
Alex Chi Z <iskyzh@gmail.com>
2023-11-16 18:09:44 -0500

    fix: MakeIterator takes read latch twice in a thread (#663)
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/storage/table/table_heap.cpp
+++ src/storage/table/table_heap.cpp
@@ -116,7 +116,9 @@ auto TableHeap::MakeIterator() -> TableIterator {
 
   auto page_guard = bpm_->FetchPageRead(last_page_id);
   auto page = page_guard.As<TablePage>();
-  return {this, {first_page_id_, 0}, {last_page_id, page->GetNumTuples()}};
+  auto num_tuples = page->GetNumTuples();
+  page_guard.Drop();
+  return {this, {first_page_id_, 0}, {last_page_id, num_tuples}};
 }
 
 auto TableHeap::MakeEagerIterator() -> TableIterator { return {this, {first_page_id_, 0}, {INVALID_PAGE_ID, 0}}; }
