b6cc75171086ef944ea4c6b434e69afc194b5897
Yuanxin Cao <60498509+xx01cyx@users.noreply.github.com>
2024-01-28 18:10:52 -0500

    fix(p1): fix file header and comments (#694)
    * fix header and misleading comments
    
    * update comments

--- src/include/storage/disk/write_back_cache.h
+++ src/include/storage/disk/write_back_cache.h
@@ -2,11 +2,11 @@
 //
 //                         BusTub
 //
-// cow_buffer.h
+// write_back_cache.h
 //
-// Identification: src/include/storage/disk/cow_buffer.h
+// Identification: src/include/storage/disk/write_back_cache.h
 //
-// Copyright (c) 2015-2023, Carnegie Mellon University Database Group
+// Copyright (c) 2015-2024, Carnegie Mellon University Database Group
 //
 //===----------------------------------------------------------------------===//
 
@@ -21,10 +21,10 @@
 namespace bustub {
 
 /**
- * WriteBackCache provides extra memory space other than the buffer pool to store the copy-on-write pages.
- * It's purpose is to gather the copy of pages that are about to be written back to disk, so that the bpm
- * doesn't have to incur IO penality and wait for the write to be completed when evicting.
- * Spring 24: The buffer is limited to store a constant number of pages in total (8).
+ * WriteBackCache provides extra memory space other than the buffer pool to store the pages. It's purpose
+ * is to gather the copy of pages that are about to be written back to disk, so that the bpm doesn't have
+ * to incur IO penality and wait for the write to be completed when evicting.
+ * Spring 24: The cache is limited to store a constant number of pages in total (8).
  * !! ANY ATTEMPTS TO ADD ANOTHER IN-MEMORY CACHE WILL BE REVIEWED MANUALLY AS PER LEADERBOARD POLICY !!
  */
 class WriteBackCache {
@@ -36,7 +36,7 @@ class WriteBackCache {
   /**
    * @brief Adds a new page to the write back cache.
    * @param page the page pointer from the bpm that is about to be evicted.
-   * @return pointer to the copied page in the buffer, or nullptr if the buffer is full.
+   * @return pointer to the copied page in the cache, or nullptr if the cache is full.
    */
   auto Add(Page *page) -> Page * {
     if ((page == nullptr) || IsFull()) {
@@ -61,10 +61,10 @@ class WriteBackCache {
   }
 
  private:
-  /** @brief Whether the buffer is full. */
+  /** @brief Whether the cache is full. */
   auto IsFull() -> bool { return free_slot_bitmap_ == 0xFFU; }
 
-  /** @brief Finds a free slot in the buffer, if not full. */
+  /** @brief Finds a free slot in the cache, if not full. */
   auto FindFreeSlot() -> uint32_t {
     BUSTUB_ASSERT(!IsFull(), "no free slot in write back cache");
     uint32_t i = 0;
