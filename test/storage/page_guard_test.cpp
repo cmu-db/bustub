f4cf381a6a3e5d06dbbc75de96dcadafe425e4bc
Connor Tsui <connor.tsui20@gmail.com>
2024-09-17 15:58:11 -0400

    clean up public-facing backend code

--- test/storage/page_guard_test.cpp
+++ test/storage/page_guard_test.cpp
@@ -6,7 +6,7 @@
 //
 // Identification: test/storage/page_guard_test.cpp
 //
-// Copyright (c) 2015-2019, Carnegie Mellon University Database Group
+// Copyright (c) 2015-2024, Carnegie Mellon University Database Group
 //
 //===----------------------------------------------------------------------===//
 
