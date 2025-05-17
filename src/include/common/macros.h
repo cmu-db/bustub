85dfe3467ccb4086ceedd2902e4235a90fc43b55
dentiny <dentinyhao@gmail.com>
2025-05-17 21:36:58 +0000

    suppress linter warning

--- src/include/common/macros.h
+++ src/include/common/macros.h
@@ -50,9 +50,9 @@ class LogFatalStream {
 
 // A macro which checks `expr` value and performs assert.
 // Different from `BUSTUB_ASSERT`, it takes stream-style parameters.
-#define BUSTUB_ASSERT_AND_LOG(expr)                       \
-  if (bool val = (expr); !val) internal::LogFatalStream { \
-      __FILE__, __LINE__                                  \
+#define BUSTUB_ASSERT_AND_LOG(expr)                       /*NOLINT*/ \
+  if (bool val = (expr); !val) internal::LogFatalStream { /*NOLINT*/ \
+      __FILE__, __LINE__                                  /*NOLINT*/ \
     }
 
 #define UNIMPLEMENTED(message) throw std::logic_error(message)
