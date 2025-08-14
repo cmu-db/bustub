0978d8c53ff2a4dbe737dc09829656b35fcf6b53
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-08-14 07:51:02 -0400

    spelling: capped
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/primer/skiplist.cpp
+++ src/primer/skiplist.cpp
@@ -108,7 +108,7 @@ SKIPLIST_TEMPLATE_ARGUMENTS void SkipList<K, Compare, MaxHeight, Seed>::Print() 
 }
 
 /**
- * @brief Generate a random height. The height should be cappped at `MaxHeight`.
+ * @brief Generate a random height. The height should be capped at `MaxHeight`.
  * Note: we implement/simulate the geometric process to ensure platform independence.
  */
 SKIPLIST_TEMPLATE_ARGUMENTS auto SkipList<K, Compare, MaxHeight, Seed>::RandomHeight() -> size_t {
