8c6c4a1d6bacda1c5d29ffa09216881afbcf7729
Melody Hu <40373356+shinyumh@users.noreply.github.com>
2025-10-10 19:07:40 -0500

    small changes

--- src/include/execution/executors/external_merge_sort_executor.h
+++ src/include/execution/executors/external_merge_sort_executor.h
@@ -22,26 +22,9 @@
 #include "execution/executors/abstract_executor.h"
 #include "execution/plans/sort_plan.h"
 #include "storage/table/tuple.h"
+#include "storage/page/intermediate_result_page.h"
 
 namespace bustub {
-
-/**
- * Page to hold the intermediate data for external merge sort.
- *
- * Only fixed-length data will be supported in Spring 2025.
- */
-class SortPage {
- public:
-  /**
-   * TODO(P3): Define and implement the methods for reading data from and writing data to the sort
-   * page. Feel free to add other helper methods.
-   */
- private:
-  /**
-   * TODO(P3): Define the private members. You may want to have some necessary metadata for
-   * the sort page before the start of the actual data.
-   */
-};
 
 /**
  * A data structure that holds the sorted tuples as a run during external merge sort.
